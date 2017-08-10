8fa38d24e2fbc3e6edec9bf0650172cd8fcd1c5b
Ronan Fitzpatrick <xronanfitzx@gmail.com>
2017-08-10 11:30:02 -0700

    Update minor errors

--- src/components/Destinations/DestinationTable.jsx
+++ src/components/Destinations/DestinationTable.jsx
@@ -35,7 +35,6 @@ DestinationTable.defaultProps = {
 
 export const mapStateToProps = (state, ownProps) => {
   const id = ownProps.widgetId;
-  console.log(id);
   const destinationIds = state.widgets.byId[id].destinations.ids;
   const destinationsById = state.widgets.byId[id].destinations.byId;
 
